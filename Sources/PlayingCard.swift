2640cb90839c8d8cd505c4a17aadc8f66a93794b
nieyu <nieyuchina@163.com>
2016-10-05 22:35:54 +0800

    init commit

---
+++ Sources/PlayingCard.swift
@@ -0,0 +1,33 @@
+struct PlayingCard {
+    let rank: Rank
+    let suid: Suit
+    
+    public init(rank: Rank, suit: Suit) {
+        self.rank = rank
+        self.suit = suit
+    }
+}
+
+// MARK: - Equatable
+
+extension PlayingCard: Equatable {}
+
+public func ==(lhs: PlayingCard, rhs: PlayingCard) -> Bool {
+    return lhs.rank == rhs.rank && lhs.suit == rhs.suit
+}
+
+// MARK: - Comparable
+
+extension PlayingCard: Comparable {}
+
+public func <(lhs: PlayingCard, rhs: PlayingCard) -> Bool {
+    return lhs.rank == rhs.rank ? lhs.suit < rhs.suit : lhs.suit > rhs.suit
+}
+
+// MARK: - CustomStringConvertible
+
+extension PlayingCard: CustomStringConvertible {
+    public var description: String {
+        return "\(suit)\(rank)"
+    }
+}
