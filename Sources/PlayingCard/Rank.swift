6806c3c1836fb7613f2a8f84f048bc560ac40dec
nieyu <nieyuchina@gmail.com>
2017-10-31 22:01:17 +0800

    fix bug4

--- Sources/PlayingCard/Rank.swift
+++ Sources/PlayingCard/Rank.swift
@@ -1,31 +1,33 @@
 public enum Rank : Int {
     case two = 2
-    case  three, four, five, six, seven, eight, nine, ten
+    case three, four, five, six, seven, eight, nine, ten
     case jack, queen, king, ace
 }
 
 // MARK: - Comparable
-
-extension Rank: Comparable {}
+extension Rank : Comparable {}
 
 public func <(lhs: Rank, rhs: Rank) -> Bool {
     switch (lhs, rhs) {
-    case (_, _) where lhs == rhs:   return false
-    case (.ace, _):                 return false
-    default:                        return lhs.rawValue < rhs.rawValue
+    case (_, _) where lhs == rhs:
+        return false
+    case (.ace, _):
+        return false
+    default:
+        return lhs.rawValue < rhs.rawValue
     }
 }
 
 // MARK: - CustomStringConvertible
-
-extension Rank: CustomStringConvertible {
+extension Rank : CustomStringConvertible {
     public var description: String {
         switch self {
-        case .ace:      return "A"
-        case .jack:     return "J"
-        case .queen:    return "Q"
-        case .King:     return "K"
-        default:        return "\(rawValue)"
+        case .ace: return "A"
+        case .jack: return "J"
+        case .queen: return "Q"
+        case .king: return "K"
+        default:
+            return "\(rawValue)"
         }
     }
 }
